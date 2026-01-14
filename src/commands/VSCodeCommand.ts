3ad42af6aea2dab23d1a8f3b19499ab9565d148a
o98k-ok <hggend@gmail.com>
2026-01-14 13:58:33 +0800

    Fix duplicate command execution in VSCodeCommand

--- src/commands/VSCodeCommand.ts
+++ src/commands/VSCodeCommand.ts
@@ -2,13 +2,13 @@ import { Command } from "../Interfaces";
 import * as vscode from "vscode";
 
 export default class VSCodeCommand implements Command {
-  constructor(private id: string, private args?: Object) {}
+  constructor(private id: string, private args?: Object) { }
 
   async execute() {
     if (this.args) {
       await vscode.commands.executeCommand(this.id, this.args);
+    } else {
+      await vscode.commands.executeCommand(this.id);
     }
-
-    await vscode.commands.executeCommand(this.id);
   }
 }
